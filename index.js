f4cc63690e2edc1e48f8878577bc26f17bc491f7
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2018-02-19 09:13:02 +0100

    re-auth after ticket expiration

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
+'use strict';
+
 const oohttp = require('oohttp');
 
 class Proxmox {
-
 	constructor(obj) {
-
 		this.port = 8006;
 		this.ticketTime = null;
+		this.ticket = null;
 
 		if (obj) {
 			Object.assign(this, obj);
 		}
 
-		this.http = new oohttp.Base();
+		this.http = new oohttp.Base({
+			url: {
+				hostname: this.hostname,
+				port: this.port,
+				protocol: 'https',
+			},
+			rejectUnauthorized: false,
+			autoContentLength: true
+		});
 		this.http.headers['content-type'] = 'application/x-www-form-urlencoded';
-		this.http.rejectUnauthorized = false;
-		this.http.autoContentLength = true;
 
 		this.Node = require('./Node.js')(this);
-
 	}
 
-	login() {
+	async request(method, reqUrl) {
+		if (!this.ticketTime || Date.now() - this.ticketTime > 3600000) {
+			await this.login(this.username, this.ticket || this.password);
+		}
+		return this.http.request(method, reqUrl);
+	}
 
-		let req = this.http.request('POST', `https://${this.hostname}:${this.port}/api2/json/access/ticket`);
-		return req.toJson({
-			password: this.password,
-			username: this.username
-		}).then((data) => {
-
-			this.ticketTime = Date.now();
-			this.http.headers.Cookie = `PVEAuthCookie=${encodeURIComponent(data.data.ticket)}`;
-			this.http.headers.CSRFPreventionToken = data.data.CSRFPreventionToken;
-
+	async login(username, password) {
+		if (username) {
+			this.username = username;
+		}
+	
+		const req = this.http.request('POST', `/api2/json/access/ticket`);
+		const data = await req.toJson({
+			username: this.username,
+			password: password || this.password
 		});
 
+		this.ticketTime = Date.now();
+		this.ticket = data.data.ticket;
+		this.http.headers.Cookie = `PVEAuthCookie=${encodeURIComponent(this.ticket)}`;
+		this.http.headers.CSRFPreventionToken = data.data.CSRFPreventionToken;
 	}
-
 }
 
 module.exports = Proxmox;
