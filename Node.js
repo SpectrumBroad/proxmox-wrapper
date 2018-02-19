67862e36ebf8c67fa5cf1decc63329ea8c5aa661
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2018-02-19 09:16:48 +0100

    wait for tasks to complete
    Before this commit, methods would resolve immediately after starting the function on promox.
    Since this commit, the methods resolve only after the task in proxmox has stopped.

--- Node.js
+++ Node.js
@@ -1,53 +1,73 @@
+'use strict';
+
 module.exports = function(PROXMOX) {
-
 	class Node {
-
 		constructor(obj) {
-
 			if (obj) {
 				Object.assign(this, obj);
 			}
 
 			this.Qemu = require('./Qemu.js')(PROXMOX, this);
-
+			this.Task = require('./Task.js')(PROXMOX, this)
 		}
 
-		getQemus() {
-
-			let req = PROXMOX.http.request('GET', `https://${PROXMOX.hostname}:${PROXMOX.port}/api2/json/nodes/${encodeURIComponent(this.name)}/qemu`);
-			return req.toObjectArray(this.Qemu);
-
-		}
-
-		getQemuByVmId(vmId) {
-
-			let req = PROXMOX.http.request('GET', `https://${PROXMOX.hostname}:${PROXMOX.port}/api2/json/nodes/${encodeURIComponent(this.name)}/qemu/${vmId}`);
-			return req.toJson().then(() => {
-				return new this.Qemu({vmId: vmId});
+		startAll(obj) {
+			return this.requestHandleUpid('POST', `/api2/json/nodes/${NODE.name}/startall`, {
+				force: !!obj.force
 			});
-
 		}
 
-		static getAll() {
+		stopAll() {
+			return this.requestHandleUpid('POST', `/api2/json/nodes/${NODE.name}/stopall`);
+		}
 
-			let req = PROXMOX.http.request('GET', `https://${PROXMOX.hostname}:${PROXMOX.port}/api2/json/nodes`);
+		async requestHandleUpid(method, reqUrl, data) {
+			return new Promise(async (resolve, reject) => {
+				try {
+					const req = await PROXMOX.request(method, reqUrl);
+					const upid = (await req.toJson(data)).data;
+					const task = await this.Task.getByUpid(upid);
+
+					let statusInterval;
+
+					const handleStatus = async () => {
+						const status = await task.getStatus();
+						if (status && status.status === 'stopped') {
+							clearInterval(statusInterval);
+
+							if (status.exitstatus === 'OK') {
+								resolve(status);
+							} else {
+								reject(new Error(status.exitstatus));
+							}
+							
+							return true;
+						}
+						return false;
+					}
+
+					if(!await handleStatus()) {
+						statusInterval = setInterval(handleStatus, 1000);
+					}
+				} catch(err) {
+					reject(err);
+				}
+			});
+		}
+
+		static async getAll() {
+			const req = await PROXMOX.request('GET', `/api2/json/nodes`);
 			return req.toObjectArray(Node);
-
 		}
 
-		static getByName(name) {
-
-			let req = PROXMOX.http.request('GET', `https://${PROXMOX.hostname}:${PROXMOX.port}/api2/json/nodes/${name}`);
-			return req.toJson().then(() => {
-				return new Node({
-					name: name
-				});
+		static async getByName(name) {
+			const req = await PROXMOX.request('GET', `/api2/json/nodes/${name}`);
+			await req.toJson();
+			return new Node({
+				name: name
 			});
-
 		}
-
 	}
 
 	return Node;
-
 };
